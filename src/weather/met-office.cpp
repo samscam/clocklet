6d2b2c2eb54a7042999bb7d1c70468337b73e1bd
Sam Easterby-Smith <me@samscam.co.uk>
2019-04-25 13:15:40 +0100

    Capital letters for weather types

--- src/weather/met-office.cpp
+++ src/weather/met-office.cpp
@@ -10,38 +10,38 @@ MetOffice::MetOffice(WiFiClient &client) : WeatherClient(client)  {
 
 
 const char* weatherTypes[] = {
-  "clear night", // 0 - clear night
-  "sunny day",             // 1 - sunny day
-  "partly cloudy night",           // 2 - Partly cloudy night
-  "partly cloudy",                 // 3 - Partly cloudy day
-  "random",                 // 4 - unused
-  "misty",                 // 5 - mist
-  "foggy",                 // 6 - fog
-  "cloudy",                // 7 - cloudy
-  "overcast",                 // 8 - overcast
-  "light showers",                 // 9 - light rain shower (night)
-  "light showers",        // 10 - light rain shower (day)
-  "drizzle",  // 11 - Drizzle
-  "light rain", // 12 - Light rain
-  "heavy showers", // 13 - Heavy rain shower (night)
-  "heavy showers",// 14 - Heavy rain shower (day)
-  "heavy rain",// 15 - Heavy rain
-  "sleet showers",// 16 - Sleet shower (night)
-  "sleet showers",// 17 - Sleet shower (day)
-  "sleet",// 18 - Sleet
-  "hail shower",// 19 - Hail shower (night)
-  "hail shower",// 20 - Hail shower (day)
-  "hail",// 21 - Hail
-  "light snow shower",// 22 - Light snow shower (night)
-  "light snow shower",// 23 - Light snow shower (day)
-  "light snow",// 24 - Light snow
-  "heavy snow shower",// 25 - Heavy snow shower (night)
-  "heavy snow shower",// 26 - Heavy snow shower (day)
-  "heavy snow",// 27 - Heavy snow
-  "thunder shower",// 28 - Thunder shower (night)
-  "thunder shower",// 29 - Thunder shower (day)
-  "thunder",// 30 - Thunder
-  "rainbows",
+  "Clear night", // 0 - clear night
+  "Sunny day",             // 1 - sunny day
+  "Partly cloudy night",           // 2 - Partly cloudy night
+  "Partly cloudy",                 // 3 - Partly cloudy day
+  "Random",                 // 4 - unused
+  "Misty",                 // 5 - mist
+  "Foggy",                 // 6 - fog
+  "Cloudy",                // 7 - cloudy
+  "Overcast",                 // 8 - overcast
+  "Light showers",                 // 9 - light rain shower (night)
+  "Light showers",        // 10 - light rain shower (day)
+  "Drizzle",  // 11 - Drizzle
+  "Light rain", // 12 - Light rain
+  "Heavy showers", // 13 - Heavy rain shower (night)
+  "Heavy showers",// 14 - Heavy rain shower (day)
+  "Heavy rain",// 15 - Heavy rain
+  "Sleet showers",// 16 - Sleet shower (night)
+  "Sleet showers",// 17 - Sleet shower (day)
+  "Sleet",// 18 - Sleet
+  "Hail showers",// 19 - Hail shower (night)
+  "Hail showers",// 20 - Hail shower (day)
+  "Hail",// 21 - Hail
+  "Light snow showers",// 22 - Light snow shower (night)
+  "Light snow showers",// 23 - Light snow shower (day)
+  "Light snow",// 24 - Light snow
+  "Heavy snow shower",// 25 - Heavy snow shower (night)
+  "Heavy snow shower",// 26 - Heavy snow shower (day)
+  "Heavy snow",// 27 - Heavy snow
+  "Thunder shower",// 28 - Thunder shower (night)
+  "Thunder shower",// 29 - Thunder shower (day)
+  "Thunder",// 30 - Thunder
+  "Rainbows",
 };
 
 
