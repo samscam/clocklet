75a3852aa740c86125b8e86a1dc2a679a5406674
Sam Easterby-Smith <me@samscam.co.uk>
2019-09-11 10:48:06 +0100

    Parse releases json from github

--- src/FirmwareUpdates/FirmwareUpdates.cpp
+++ src/FirmwareUpdates/FirmwareUpdates.cpp
@@ -34,18 +34,32 @@ void FirmwareUpdates::checkForUpdates() {
     
             // file found at server
             if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-                String payload = https.getString();
-                Serial.println(payload);
-                // Parse the json
+                // Capacity with 10k overhead - should be more than plenty
+                const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(13) + 3*JSON_OBJECT_SIZE(18) + 10240;
+                DynamicJsonDocument doc(capacity);
+
+                // Parse JSON object
+                DeserializationError error = deserializeJson(doc, *client);
+                if (error) {
+                    Serial.print(F("deserializeJson() failed: "));
+                    Serial.println(error.c_str());
+                    return;
+                }
 
                 // Fish out verion string
+                const char* tag_name = doc["tag_name"];
+                Serial.printf("[FIRMWARE UPDATES] Latest version is %s\n",tag_name);
+                Serial.printf("[FIRMWARE UPDATES] Local version is %s\n",VERSION);
 
                 // Compare that to the local version
 
                 // if local < latest
 
                 // Fish out the binary url
-
+                JsonObject assets_0 = doc["assets"][0];
+                const char* downloadURL = assets_0["browser_download_url"];
+                Serial.printf("[FIRMWARE UPDATES] Download URL is %s\n",downloadURL);
+                
                 // Perform the update
 
             }
