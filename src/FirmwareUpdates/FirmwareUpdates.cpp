a834668568f2d6b1d5334737a76e293e751f6af8
Sam Easterby-Smith <me@samscam.co.uk>
2019-09-11 10:52:29 +0100

    Removed unused includes

--- src/FirmwareUpdates/FirmwareUpdates.cpp
+++ src/FirmwareUpdates/FirmwareUpdates.cpp
@@ -5,8 +5,6 @@
 #include <ArduinoJson.h>
 
 #include "FirmwareUpdates.h"
-#include <functional>
-#include <map>
 
 void FirmwareUpdates::checkForUpdates() {
     WiFiClientSecure *client = new WiFiClientSecure;
@@ -34,7 +32,8 @@ void FirmwareUpdates::checkForUpdates() {
     
             // file found at server
             if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-                // Capacity with 10k overhead - should be more than plenty
+
+                // JSON parsing capacity with 10k overhead - should be more than plenty
                 const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(13) + 3*JSON_OBJECT_SIZE(18) + 10240;
                 DynamicJsonDocument doc(capacity);
 
@@ -59,7 +58,7 @@ void FirmwareUpdates::checkForUpdates() {
                 JsonObject assets_0 = doc["assets"][0];
                 const char* downloadURL = assets_0["browser_download_url"];
                 Serial.printf("[FIRMWARE UPDATES] Download URL is %s\n",downloadURL);
-                
+
                 // Perform the update
 
             }
