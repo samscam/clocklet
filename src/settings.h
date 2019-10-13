2e4e8f962f79f79c1b01deb4d9ccbd7236fe0fd1
Sam Easterby-Smith <me@samscam.co.uk>
2019-10-13 21:24:56 +0100

    Added map of preferences

--- src/settings.h
+++ src/settings.h
@@ -1,7 +1,20 @@
-#ifndef PRISCILLA_SETTINGS
-#define PRISCILLA_SETTINGS
+#pragma once
 
 #define FPS               60
 #define TIME_24_HOUR      true
 
-#endif
+// MAP FOR PREFERENCES - keeping it here for now...
+  // preferences.putString("owner","people");
+  // preferences.putUInt("serial",1);
+  // preferences.putUInt("hwrev",2); // hardware rev
+  // preferences.putUInt("swmigrev",VERSION) // last fw version used - update after migrations!
+  // preferences.putUInt("casecolour",0);
+  // enum CaseColour {
+  //   black = 0,
+  //   wood = 1,
+  //   rainbow = 2
+  // }
+
+  // Location
+  // preferences.putDouble("lat",0);
+  // preferences.putDouble("lng",0);
