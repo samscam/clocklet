1aa8abed030dfdb97732f2a5b1ef68d6fd107a2f
Sam Easterby-Smith <me@samscam.co.uk>
2019-11-17 23:51:41 +0000

    Too much fun making redirects work properly (and pointers)

--- src/FirmwareUpdates/FirmwareUpdates.cpp
+++ src/FirmwareUpdates/FirmwareUpdates.cpp
@@ -8,242 +8,262 @@
 #include <semver.h>
 #include "settings.h"
 
-FirmwareUpdates::FirmwareUpdates() {
-  this->client = new WiFiClientSecure;
-};
+#include "esp_log.h"
 
-/** 
- * Checks for firmware updates
- * If a valid firmware image is found it will apply the update
- * Ensure that the network is connected before running this
- * */
+const char* TAG = "Firmware Updates";
 
-void FirmwareUpdates::checkForUpdates() {
+#define MAX_REDIRECT_DEPTH 5
+
+FirmwareUpdates::FirmwareUpdates() {};
+
+// Pass it a url... it will perform a get, process redirects
+bool FirmwareUpdates::_getWithRedirects(HTTPClient ** httpsptr, WiFiClientSecure ** clientptr, const char* url, int depth){
+
+    if (depth > MAX_REDIRECT_DEPTH){
+        ESP_LOGE(TAG, "Maximum redirects exceeded");
+        return false;
+    }
     
+    ESP_LOGV(TAG, "IN: httpsptr %p ... clientptr %p",httpsptr,clientptr);
+    ESP_LOGV(TAG, "deref: httpsptr %p ... clientptr %p",*httpsptr,*clientptr);
+
+    WiFiClientSecure *client = new WiFiClientSecure();
+    HTTPClient *https = new HTTPClient();
+
+    *clientptr = client;
+    *httpsptr = https;
+
+    ESP_LOGV(TAG, "created: https %p ... client %p",https,client);
+    ESP_LOGV(TAG, "assigned: httpsptr %p ... clientptr %p",httpsptr,clientptr);
+    ESP_LOGV(TAG, "deref: httpsptr %p ... clientptr %p",*httpsptr,*clientptr);
+
+    ESP_LOGV(TAG, "BEGIN");
+    // WiFiClient& upcast = &*client;
+    if (!https->begin(*client, url)) {
+        ESP_LOGE(TAG, "Could not begin HTTPS request to: %s",url);
+        delete client;
+        delete https;
+        return false;
+    }
+
+    const char * headerKeys[] = {"Location","Content-Length","Content-Type"};
+    https->collectHeaders(headerKeys,3);
+    ESP_LOGV(TAG, "Get");
+    int httpCode = https->GET();
+    ESP_LOGV(TAG, "nonNeg");
+    // httpCode will be negative on error
+    if (httpCode < 0) {
+        ESP_LOGE(TAG, "HTTP error: %s\n-- URL: %s", *https->errorToString(httpCode).c_str(), url);
+        delete client;
+        delete https;
+        return false;
+    }
+    ESP_LOGV(TAG, "Redirects");
+    // Handle redirects
+    if (httpCode >= 300 && httpCode < 400 ){
+        if (https->hasHeader("Location")) {
+            String redirectLocation = https->header("Location");
+            
+            ESP_LOGV(TAG, "Flushola");
+            https->end();
+            client->flush();
+
+            delete client;
+            delete https;
+        
+            ESP_LOGI(TAG, "Redirecting: %s", redirectLocation.c_str());
+            return _getWithRedirects(&*httpsptr, &*clientptr, redirectLocation.c_str(), depth++);
+        } else {
+            ESP_LOGE(TAG, "%d status code but no redirect location", httpCode);
+            delete client;
+            delete https;
+            return false;
+        }
+    }
+    ESP_LOGV(TAG, "TWO HUNDRED");
+    // Handle other non-200 status codes
+    if (httpCode != 200){
+        ESP_LOGE(TAG, "Bailing out due to %d status code", httpCode);
+        delete client;
+        delete https;
+        return false;
+    }
+
+    return true;
+
+}
+
+bool FirmwareUpdates::checkForUpdates() {
+    
+    ESP_LOGI(TAG, "Checking for firmware updates");
+
     updateAvailable = false;
 
-    if (!client) {
-        Serial.println("[FIRMWARE UPDATES] failed to create network client...");
-        return;
+    // Fetch latest release data from github
+    // GET /repos/:owner/:repo/releases/latest
+    HTTPClient* https = nullptr;
+    WiFiClientSecure* client = nullptr;
+    ESP_LOGV(TAG, "About to GET! %p ... %p",https,client);
+    if (!_getWithRedirects(&https, &client, "https://api.github.com/repos/samscam/priscilla/releases/latest")){
+        return false;
     }
 
-    // Extra scoping block
-    {
-        HTTPClient https;
-        Serial.print("[FIRMWARE UPDATES] begin...\n");
+    // JSON parsing capacity with 10k overhead - should be more than plenty
+    const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(13) + 3*JSON_OBJECT_SIZE(18) + 10240;
+    DynamicJsonDocument doc(capacity);
 
-        // Fetch latest release data from github
-        // GET /repos/:owner/:repo/releases/latest
+    // Parse JSON object
+    DeserializationError error = deserializeJson(doc, *client);
 
-        if (https.begin(*client, "https://api.github.com/repos/samscam/priscilla/releases/latest")) {  // HTTPS
-            Serial.print("[FIRMWARE UPDATES] Fetching latest version from github...\n");
-            // start connection and send HTTP header
-            int httpCode = https.GET();
-    
-            // httpCode will be negative on error
-            if (httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            Serial.printf("[FIRMWARE UPDATES] GET... code: %d\n", httpCode);
-    
-            // file found at server
-            if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+    ESP_LOGV(TAG, "TRASHING");
+    delete client;
+    delete https;
 
-                // JSON parsing capacity with 10k overhead - should be more than plenty
-                const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(13) + 3*JSON_OBJECT_SIZE(18) + 10240;
-                DynamicJsonDocument doc(capacity);
-
-                // Parse JSON object
-                DeserializationError error = deserializeJson(doc, *client);
-                if (error) {
-                    Serial.print(F("[FIRMWARE UPDATES] deserializeJson() failed: "));
-                    Serial.println(error.c_str());
-                    return;
-                }
-
-                https.end();
-
-                // Fish out verion string
-                const char* tag_name = doc["tag_name"];
-                
-                // Strip the v from the start of the tag - if it doesn't have one, bail out
-                int taglen = strlen(tag_name);
-                char latestbuf[taglen];
-                memcpy(latestbuf, &tag_name[1],taglen);
-
-                semver_t latest = {};
-                if (semver_parse(latestbuf, &latest)){
-                    Serial.print("[FIRMWARE UPDATES] Failed to parse latest version");
-                    return;
-                }
-                Serial.printf("[FIRMWARE UPDATES] Latest firmware is %s\n", latestbuf);
-
-                semver_t local = {};
-                if ( semver_parse(VERSION, &local)){
-                    Serial.print("[FIRMWARE UPDATES] Failed to parse local version");
-                    return;
-                }
-                Serial.printf("[FIRMWARE UPDATES] Local firmware is %s\n", VERSION);
-
-                // Compare that to the local version
-                // Bail unless an update is needed
-                int comparison = semver_compare(local,latest);
-                if (comparison == 0) {
-                    Serial.println("[FIRMWARE UPDATES] Local firmware is up to date");
-                    return;
-                } else if (comparison > 0) {
-                    Serial.println("[FIRMWARE UPDATES] Local firmware is newer than latest - must be developing");
-                    return;
-                } else {
-                    Serial.println("[FIRMWARE UPDATES] Latest is newer. Firmware update will be triggered");
-                }
-
-                // Fish out the binary url
-                JsonObject assets_0 = doc["assets"][0];
-                strcpy(downloadURL,assets_0["browser_download_url"]);
-                Serial.printf("[FIRMWARE UPDATES] Download URL is %s\n",downloadURL);
-                updateAvailable = true;
-                
-            }
-            } else {
-            Serial.printf("[FIRMWARE UPDATES][HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
-            }
-    
-            https.end();
-        } else {
-            Serial.printf("[FIRMWARE UPDATES][HTTPS] Unable to connect\n");
-        }
-
+    if (error) {
+        ESP_LOGE(TAG, "deserializeJson() failed: %s", error.c_str());
+        return false;
     }
+    ESP_LOGV(TAG, "FISHING");
+
+    // Fish out verion string
+    const char* tag_name = doc["tag_name"];
+    
+    // Strip the v from the start of the tag - if it doesn't have one, bail out
+    int taglen = strlen(tag_name);
+    char latestbuf[taglen];
+    memcpy(latestbuf, &tag_name[1],taglen);
+
+    semver_t latest = {};
+    if (semver_parse(latestbuf, &latest)){
+        ESP_LOGE(TAG, "Failed to parse latest version");
+        return false;
+    }
+    ESP_LOGI(TAG, "Latest firmware is %s", latestbuf);
+
+    semver_t local = {};
+    if ( semver_parse(VERSION, &local)){
+        ESP_LOGE(TAG, "Failed to parse local version");
+        return false;
+    }
+    ESP_LOGI(TAG, "Local firmware is %s", VERSION);
+
+    // Compare that to the local version
+    // Bail unless an update is needed
+    int comparison = semver_compare(local,latest);
+    if (comparison == 0) {
+        ESP_LOGI(TAG, "Local firmware is up to date");
+        return true;
+    } else if (comparison > 0) {
+        ESP_LOGI(TAG, "Local firmware is newer than latest - must be developing");
+        return true;
+    } else {
+        ESP_LOGI(TAG, "Latest is newer. Firmware update will be triggered");
+    }
+
+    // Fish out the binary url
+    if (doc["assets"][0].isNull()){
+        ESP_LOGE(TAG, "Assets section not found in payload");
+        return false;
+    }
+    JsonObject assets_0 = doc["assets"][0];
+
+    if (assets_0["browser_download_url"].isNull()){
+        ESP_LOGE(TAG, "Download URL not found in payload");
+        return false;
+    }
+    const char * __downloadURL = assets_0["browser_download_url"];
+
+    strcpy(_downloadURL,__downloadURL);
+    ESP_LOGI(TAG, "Download URL is %s\n",_downloadURL);
+    updateAvailable = true;
+
+    return true;
 
 }
 
-void FirmwareUpdates::startUpdate(){
-    if (updateAvailable && downloadURL){
-        processOTAUpdate(downloadURL);
+bool FirmwareUpdates::startUpdate(){
+    if (updateAvailable && _downloadURL){
+        return _processOTAUpdate(_downloadURL);
     }
+    return false;
 }
 
-void FirmwareUpdates::processOTAUpdate(const char * url)
+bool FirmwareUpdates::_processOTAUpdate(const char * url)
 {
-    Serial.printf("[FIRMWARE UPDATES] About to download from %s\n",url);
-    if (!client) {
-        Serial.println("[FIRMWARE UPDATES] failed to create network client...");
-        return;
-    }
-    {
-        HTTPClient https;
-
-        if (https.begin(*client, url)) {  // HTTPS
-            Serial.println("[FIRMWARE UPDATES] Starting firmware load");
-
-            const char * headerKeys[] = {"Location","Content-Length","Content-Type"};
-            https.collectHeaders(headerKeys,3);
-
-            // start connection and send HTTP header
-            int httpCode = https.GET();
-
-            // httpCode will be negative on error
-            if (httpCode <= 0) {
-                Serial.println("[FIRMWARE UPDATES] Error from httpclient");
-                return;
-            }
-
-            // HTTP header has been sent and Server response header has been handled
-            Serial.printf("[FIRMWARE UPDATES][HTTPS] GET... code: %d\n", httpCode);
-
-
-            // Handle redirects
-            if (httpCode >= 300 && httpCode < 400 ){
-                if (https.hasHeader("Location")) {
-                    String redirectLocation = https.header("Location");
-                    https.end();
-                    Serial.println("[FIRMWARE UPDATES] Redirect: " + redirectLocation);
-                    processOTAUpdate(redirectLocation.c_str()); // should we limit redirects? we should not recreate another wificlient?
-                    return;
-                } else {
-                    Serial.println("[FIRMWARE UPDATES] 3xx status code but no redirect location");
-                    return;
-                }
-            }
-
-            // Handle other non-200 status codes
-            if (httpCode != 200){
-                Serial.println("[FIRMWARE UPDATES] Bailing out due to non-200 status code");
-                return;
-            }
-            
-            volatile int contentLength = 0;
-
-            if (https.hasHeader("Content-Length")){
-                contentLength = atoi(https.header("Content-Length").c_str());
-                Serial.printf("[FIRMWARE UPDATES] Content length is %i",contentLength);
-            } else {
-                Serial.println("[FIRMWARE UPDATES] No content length found");
-                return;
-            }
-
-            volatile bool isValidContentType = false;
-            if (https.hasHeader("Content-Type")){
-                String contentType = https.header("Content-Type");
-                if ( contentType == "application/octet-stream"){
-                    isValidContentType = true;
-                }
-                Serial.println("[FIRMWARE UPDATES] Content type is " + contentType);
-            } else {
-                Serial.println("[FIRMWARE UPDATES] No content type found");
-                return;
-            }
-
-            // WE ARE GETTING SOMEWHERE
-
-            // check whether we have everything for OTA update
-            if (contentLength && isValidContentType)
-            {
-                if (Update.begin(contentLength))
-                {
-                Serial.println("[FIRMWARE UPDATES] Starting Over-The-Air update. This may take some time to complete ...");
-                size_t written = Update.writeStream(*client);
-
-                if (written == contentLength)
-                {
-                    Serial.println("[FIRMWARE UPDATES] Written : " + String(written) + " successfully");
-                }
-                else
-                {
-                    Serial.println("[FIRMWARE UPDATES] Written only : " + String(written) + "/" + String(contentLength) + ". Retry?");
-                    // Retry??
-                }
-
-                if (Update.end())
-                {
-                    if (Update.isFinished())
-                    {
-                    Serial.println("[FIRMWARE UPDATES] OTA update has successfully completed. Rebooting ...");
-                    ESP.restart();
-                    }
-                    else
-                    {
-                    Serial.println("[FIRMWARE UPDATES] Something went wrong! OTA update hasn't been finished properly.");
-                    }
-                }
-                else
-                {
-                    Serial.println("[FIRMWARE UPDATES] An error Occurred. Error #: " + String(Update.getError()));
-                }
-                }
-                else
-                {
-                Serial.println("[FIRMWARE UPDATES] There isn't enough space to start OTA update");
-                client->flush();
-                }
-            }
-            else
-            {
-                Serial.println("[FIRMWARE UPDATES] There was no valid content in the response from the OTA server!");
-                client->flush();
-            }
-        }
-            
+    ESP_LOGI(TAG, "About to download from %s",url);
+    
+    HTTPClient* https = nullptr;
+    WiFiClientSecure* client = nullptr;
+    
+    if (!_getWithRedirects(&https, &client, url)){
+        return false;
     }
 
+            
+    volatile int contentLength = 0;
+
+    if (https->hasHeader("Content-Length")){
+        contentLength = atoi(https->header("Content-Length").c_str());
+        ESP_LOGD(TAG, "Content length is %i",contentLength);
+    } else {
+        ESP_LOGE(TAG, "No content length found");
+        delete client;
+        delete https;
+        return false;
+    }
+
+    if (!https->hasHeader("Content-Type")){
+        ESP_LOGE(TAG, "No content type found");
+        delete client;
+        delete https;
+        return false;
+    }
+
+    String contentType = https->header("Content-Type");
+    if (contentType != "application/octet-stream") {
+        ESP_LOGE(TAG, "Invalid content type: %s", contentType);
+        delete client;
+        delete https;
+        return false;
+    }
+    ESP_LOGD(TAG, "Content type is %s", contentType);
+
+
+    if (!Update.begin(contentLength)) {
+        ESP_LOGE(TAG, "OTA could not start update - probably not enough free space");
+        delete client;
+        delete https;
+        return false;
+    }
+
+    ESP_LOGI(TAG, "Starting Over-The-Air update. This may take some time to complete ...");
+    size_t written = Update.writeStream(*client);
+
+    if (written == contentLength) {
+        ESP_LOGI(TAG, "Written : %d successfully", written);
+    } else {
+        ESP_LOGE(TAG, "Written only : %d/%d " ,written, contentLength);
+        delete client;
+        delete https;
+        return false;
+    }
+
+    if (!Update.end()) {
+        ESP_LOGE(TAG, "An error Occurred. Error #: %d", Update.getError());
+        delete client;
+        delete https;
+        return false;
+    }
+
+    if (!Update.isFinished()) {
+        ESP_LOGE(TAG, "Something went wrong! OTA update hasn't been finished properly.");
+        delete client;
+        delete https;
+        return false;
+    }
+
+    ESP_LOGI(TAG, "OTA update has successfully completed. Rebooting ...");
+    ESP.restart();
+
+    return true; // But this will never ever happen :)
 }
--- src/FirmwareUpdates/FirmwareUpdates.h
+++ src/FirmwareUpdates/FirmwareUpdates.h
@@ -1,15 +1,46 @@
 #pragma once
 
+#include <HTTPClient.h>
 #include <WiFiClientSecure.h>
 
 class FirmwareUpdates {
     public:
     FirmwareUpdates();
-    void checkForUpdates();
-    void startUpdate();
+
+    /**
+     * Checks for firmware updates
+     * 
+     * Fetches release metadata from github and compares
+     * the latest remote version to the local version
+     * 
+     * Returns true if there were no errors, false on error
+     * 
+     * will set the `updateAvailable` flag if there is anything new
+     * and the (private) `downloadURL` variable
+     */
+
+    bool checkForUpdates();
+
+    /**
+     * Starts the update process
+     * ... run checkForUpdate before calling this
+     * on sucessful completion, the system will be rebooted
+     * OTHERWISE it will return false
+     */
+
+    bool startUpdate();
+
+    /**
+     * indicates whether an update is available after calling `checkForUpdates()`
+     */
+
     bool updateAvailable;
+
+
     private:
-    void processOTAUpdate(const char* url);
-    char downloadURL[2048];
-    WiFiClientSecure *client;
+
+    bool _processOTAUpdate(const char* url);
+    char _downloadURL[2048];
+    bool _getWithRedirects(HTTPClient ** httpsptr, WiFiClientSecure ** clientptr, const char* url, int depth = 0);
+
 };
