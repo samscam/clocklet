c2866d0bff226eba4ce25b964bb98f6354c58983
Sam Easterby-Smith <me@samscam.co.uk>
2020-05-16 00:14:11 +0100

    Simplified time fetching from ESP32 rtc so it's non-blocking

--- src/TimeThings/ESP32Rtc.cpp
+++ src/TimeThings/ESP32Rtc.cpp
@@ -2,7 +2,7 @@
 
 #include "ESP32Rtc.h"
 #include "time.h"
-
+#include "lwip/apps/sntp.h"
 
 boolean RTC_ESP32::begin(void){
   configTime(0*3600,0*3600,"pool.ntp.org");
@@ -13,13 +13,9 @@ void RTC_ESP32::adjust(const DateTime& dt){
 }
 
 DateTime RTC_ESP32::now(){
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("failed to obtain time");
-    return DateTime(0);
-  }
-
-  return DateTime(timeinfo.tm_year,timeinfo.tm_mon,timeinfo.tm_mday,timeinfo.tm_hour,timeinfo.tm_min,timeinfo.tm_sec);
+  timeval tv;
+  gettimeofday(&tv,NULL);
+  return DateTime(tv.tv_sec);
 }
 
 /* ESP32 calibration experiment
