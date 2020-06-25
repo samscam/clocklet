b2bf42eaa17930a8712592701d8a711ffe99dcab
Sam Easterby-Smith <me@samscam.co.uk>
2020-06-25 10:07:19 +0100

    Inject the Cental into the Clocklist

--- Clockses/Clockses/Business/SceneDelegate.swift
+++ Clockses/Clockses/Business/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     
-    let clockList = ClockList()
+    let clockList = ClockList(central:Central())
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
--- Clockses/Clockses/Clock List/ClockListView.swift
+++ Clockses/Clockses/Clock List/ClockListView.swift
@@ -12,43 +12,93 @@ import SwiftUI
 import Combine
 import CombineBluetooth
 
+struct ScanningView: View{
+    var scanning: Bool
+    
+    @State private var animationAmount = 0.3
+    
+    var body: some View{
+        VStack{
+            if scanning {
+                Image(systemName: "eye").opacity(scanning ? animationAmount : 0.5).animation(
+                    Animation.easeInOut(duration: 1)
+                    .repeatForever())
+                    .onAppear(){
+                        self.animationAmount = 0.6
+                    }
+                    
+                Text("Scanning for clocks").opacity(0.5)
+                    
+                
+            } else {
+                Image(systemName: "eye.slash").opacity(0.5)
+                Text("Not scanning...").opacity(0.5)
+            }
+        }
+        
+    }
+}
+
 struct ClockListView: View {
     @EnvironmentObject var clockList: ClockList
+    
+    init(){
+        
+    }
     
     var body: some View {
         NavigationView{
             VStack{
-                List(clockList.clocks) { clock in
 
-                    NavigationLink(destination:
-                    ClockDetailsView().environmentObject(clock)){
-                        ClockSummaryView().environmentObject(clock)
-                    }.listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 40))
-                }
+                    List(clockList.clocks) { clock in
+                        NavigationLink(destination:
+                                        ClockDetailsView().environmentObject(clock)){
+                            ClockSummaryView().environmentObject(clock)
+                        }
+                        
+                    }.background(Color.blue)
+
                 
-                if clockList.isScanning {
-                    Text("Scanning").background(Color.green)
-                }
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 40))
+                
+                Spacer(minLength: 30)
+                
+                ScanningView(scanning: clockList.isScanning)
+                
+                 Spacer(minLength: 30)
             }.navigationBarTitle(Text("Clocks"))
-            .onAppear {
-                self.clockList.disconnectAllDevices()
-                self.clockList.startScanning()
-            }.onDisappear(){
-                self.clockList.stopScanning()
-            }
+
+        }.navigationViewStyle(StackNavigationViewStyle())
+        .onAppear {
+            self.clockList.disconnectAllDevices()
+            self.clockList.startScanning()
+        }.onDisappear(){
+            self.clockList.stopScanning()
         }
+
     }
 
 }
 
-//struct ClockListView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        let listView = ClockListView()
-//        listView.viewModel.clockItems = [
-//            ClockSummaryViewModel(clock: Clock("Clock One", .black)),
-//            ClockSummaryViewModel(clock: Clock("Clock Two", .wood)),
-//            ClockSummaryViewModel(clock: Clock("Clock Three", .bones))
-//        ]
-//        return listView
-//    }
-//}
+
+struct ClockListView_Previews: PreviewProvider {
+    
+
+    static let clockList: ClockList = {
+        let clockList = ClockList(central:nil)
+        clockList.clocks = [
+            Clock("One", .black),
+            Clock("Two", .blue)
+        ]
+        return clockList
+    }()
+    
+    
+    static var previews: some View {
+        Group{
+            ClockListView().environmentObject(clockList)
+        ScanningView(scanning: true)
+        }
+        
+    }
+}
