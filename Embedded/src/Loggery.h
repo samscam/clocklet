e4a8d2492dbbb675a49b3c237270442694a9fa4e
Sam Easterby-Smith <me@samscam.co.uk>
2022-12-06 22:32:59 +0000

    More detail when logging memory usage

--- Embedded/src/Loggery.h
+++ Embedded/src/Loggery.h
@@ -2,5 +2,5 @@
 #include <Esp.h>
 #include <esp_log.h>
 
-#define LOGMEM ESP_LOGI("MEM","FreeMem %d -- line %d - %s - %s",ESP.getFreeHeap(),__LINE__,__func__,__FILE__)
+#define LOGMEM ESP_LOGI("MEM","High water %d --- FreeMem %d -- line %d - %s - %s",uxTaskGetStackHighWaterMark(NULL),ESP.getFreeHeap(),__LINE__,__func__,__FILE__)
 
