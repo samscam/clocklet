04611981827f39ee39c99b8ef1bd6697b94b1680
Sam Easterby-Smith <me@samscam.co.uk>
2022-12-07 18:18:23 +0000

    Some random changes to the ConfigItemView layout

--- Clients/iOS/Clocklet/Clocklet/Shared Views/ConfigItemView.swift
+++ Clients/iOS/Clocklet/Clocklet/Shared Views/ConfigItemView.swift
@@ -30,25 +30,31 @@ struct ConfigItemView<Content:View>: View {
             Spacer()
             HStack(alignment: .center,spacing:10){
                 
-                icon.resizable().scaledToFit().frame(width: 30, height: 40, alignment: .center).foregroundColor(self.iconColor ?? Color("Black-ish"))
+                icon.resizable().scaledToFit().frame(maxWidth: 40, alignment: .center).foregroundColor(self.iconColor ?? Color("Black-ish"))
                 Text(title).font(.title).bold().lineLimit(4).foregroundColor(Color("Black-ish"))
                 Spacer()
                 if (disclosure){
                     Image(systemName: "chevron.right")
                 }
-            }.frame(maxWidth: .infinity)
+            }
+            .frame(minWidth:0, maxWidth: .infinity)
+            .contentShape(Rectangle())
             
-            content().padding(.leading, 40)
+            content()
+                .fixedSize(horizontal: false, vertical: true)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(.leading, 50)
             
-        }.frame(minWidth: 0, maxWidth: .infinity,  alignment: Alignment.topLeading)
-        .contentShape(Rectangle())
+        }.frame( maxWidth: .infinity,  maxHeight: .infinity, alignment: Alignment.topLeading)
+        
+        
     }
 }
 
 
 struct ConfigItemView_Previews: PreviewProvider {
     static var previews: some View {
-        Group{
+        VStack{
             ConfigItemView(icon: Image(systemName: "link.circle.fill"), title: "Hello"){EmptyView()}
             
             ConfigItemView(icon: Image(systemName: "trash.fill"), title: "Throw it away"){
@@ -57,13 +63,14 @@ Nullam quis risus eget urna mollis ornare vel eu leo. Etiam porta sem malesuada 
 
 Maecenas faucibus mollis interdum. Cras mattis consectetur purus sit amet fermentum. Nullam id dolor id nibh ultricies vehicula ut id elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.
 """)
+                
             }
             
             ConfigItemView(icon: Image(systemName: "smiley"), title: "Hello"){
                 Text("I love you")
             }
             
-        }.previewLayout(.sizeThatFits)
+        }.padding()
         
     }
 }
