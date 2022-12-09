19296f5b547e97f32dbc3a691f4218a0895b162d
Sam Easterby-Smith <me@samscam.co.uk>
2022-12-09 22:11:20 +0000

    iOS: Oh my! No more AppDelegate

---
+++ Clients/iOS/Clocklet/Clocklet/Business/ClockletApp.swift
@@ -0,0 +1,20 @@
+//
+//  ClockletApp.swift
+//  Clocklet
+//
+//  Created by Sam Easterby-Smith on 10/05/2019.
+//  Copyright © 2019 Spotlight Kid Ltd. All rights reserved.
+//
+
+import SwiftUI
+import CombineBluetooth
+
+@main
+struct ClockletApp: App{
+    let clockList = ClockList(central:Central())
+    
+    var body: some Scene {
+        WindowGroup{ ClockListView().environmentObject(clockList)
+        }
+    }
+}
