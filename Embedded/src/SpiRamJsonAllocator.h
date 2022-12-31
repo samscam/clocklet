4300b6c8b602912a2a83b8b804a089e5acfe0b42
Sam Easterby-Smith <me@samscam.co.uk>
2022-12-31 00:13:37 +0000

    Embedded: SpiRamJsonDocument should have a reallocate method

--- Embedded/src/SpiRamJsonAllocator.h
+++ Embedded/src/SpiRamJsonAllocator.h
@@ -11,6 +11,9 @@ struct SpiRamAllocator {
   void deallocate(void* pointer) {
     heap_caps_free(pointer);
   }
+  void* reallocate(void* ptr, size_t new_size) {
+    return heap_caps_realloc(ptr, new_size, MALLOC_CAP_SPIRAM);
+  }
 };
 
 using SpiRamJsonDocument = BasicJsonDocument<SpiRamAllocator>;
